9e81267312878ec8803cfea895d666a5073fa3d7
Nathy-bajo <bajon7680@gmail.com>
2026-05-26 12:14:14 +0000

    resolve conflict

--- substrate/frame/revive/rpc/src/lib.rs
+++ substrate/frame/revive/rpc/src/lib.rs
@@ -25,7 +25,8 @@ use jsonrpsee::{
 	types::{ErrorCode, ErrorObjectOwned},
 };
 use pallet_revive::evm::*;
-use sp_core::{H160, H256, U256, keccak_256};
+use sp_core::{H160, H256, U256};
+use sp_crypto_hashing::keccak_256;
 use std::pin::Pin;
 use thiserror::Error;
 use tokio_stream::wrappers::{BroadcastStream, errors::BroadcastStreamRecvError};
